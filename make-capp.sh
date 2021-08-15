b277e9206f194f0bef87c5ec7fb0cd9530a549bd
Michael Bach <michael.bach@uni-freiburg.de>
2021-08-15 23:17:25 +0200

    2021-08-15 sequence change in "make-capp", renaming "Show center fixation mark"

--- make-capp.sh
+++ make-capp.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
-# 2021-02-04 began
+# 2021-08-01 moved "rm …capp" before "jake…"
+# 2021-02-04 begun
 
 cd ${0:a:h} # go to the starting directory
 # pwd
+rm -R ../capp
 jake release # compile to release stage
 rm -R Build/Release/capp/Frameworks # we don't need this
 rm -R Build/Release/capp/CommonJS.environment # nor this
-rm -R ../capp
 mv Build/Release/capp ../ # move it up, creating the "capp" folder
 rm -R Build # and get rid of the rest of the built items
 
