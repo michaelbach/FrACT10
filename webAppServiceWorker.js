bc4918cb6bfa2e86cae32ab5fc514fd32b6c1c1f
Michael Bach <michael.bach@uni-freiburg.de>
2023-07-05 12:30:56 +0200

    override not-understood git error

--- webAppServiceWorker.js
+++ webAppServiceWorker.js
@@ -1,7 +1,7 @@
 /* file "webAppServiceWorker.js" */
 
 
-const cacheName = 'FrACT10-sw-2023-07-03';
+const cacheName = 'FrACT10-sw-2023-07-05';
 
 /* Fetching content using Service Worker */
 self.addEventListener('fetch', (e) => {
