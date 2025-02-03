bc66000fe04df12d312ef3b28aaa2cbad929bea0
Michael Bach <michael.bach@uni-freiburg.de>
2025-02-03 09:20:38 +0100

    deploy

--- webAppServiceWorker.js
+++ webAppServiceWorker.js
@@ -2,7 +2,7 @@
 // file "webAppServiceWorker.js"
 //
 
-const cacheName = "FrACT10·2025-02-02";
+const cacheName = "FrACT10·2025-02-03";
 
 
 // Fetching content using Service Worker, this is called on reload. If cache name has changed, `install` is next
