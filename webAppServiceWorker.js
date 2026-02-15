81acbb20f883e8f697ec038f85052e80d17233a8
Michael Bach <michael.bach@uni-freiburg.de>
2026-02-15 12:26:03 +0100

    GUI: improve aesthetics in Settings▸Misc

--- webAppServiceWorker.js
+++ webAppServiceWorker.js
@@ -2,7 +2,7 @@
 //file "webAppServiceWorker.js"
 //
 
-const cacheName = "FrACT10·2026-02-14";
+const cacheName = "FrACT10·2026-02-15";
 
 
 //Fetching content using Service Worker, this is called on reload. If cache name has changed, `install` is next
